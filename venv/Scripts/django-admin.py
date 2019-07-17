d2b521c35b6864840457246b0fd0fbe50130d211
pandadevoper <the1ime@yandex.ru>
2019-07-17 15:38:53 +0500

    fix venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\coding\freelance_projects\free\create_notification_app_django\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
